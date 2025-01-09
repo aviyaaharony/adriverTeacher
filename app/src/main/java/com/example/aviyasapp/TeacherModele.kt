885f779def01138cc054c9771250bfee770fa0d0
aviya <aviyaaharony@gmail.com>
2025-01-09 16:28:24 +0200

    19.12

---
+++ app/src/main/java/com/example/aviyasapp/TeacherModele.kt
@@ -0,0 +1,8 @@
+package com.example.aviyasapp
+
+class TeacherModel {
+    private var name: String? = null
+    private var students = ArrayList<Student>()
+    private var hourOfWork = ArrayList<String>()
+    private var price: Int = 0
+}
