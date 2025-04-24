237f3bfff790d3ff377ccc39fc0693df358b0d6d
aviya <aviyaaharony@gmail.com>
2025-04-24 16:32:16 +0300

    20.04

--- app/src/main/java/com/example/aviyasapp/View/pickTeacher.kt
+++ app/src/main/java/com/example/aviyasapp/View/pickTeacher.kt
@@ -2,11 +2,21 @@ package com.example.aviyasapp.View
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.aviyasapp.Model.TeacherModel
 import com.example.aviyasapp.R
 import com.example.aviyasapp.adapters.CustomAdapter
+import com.example.aviyasapp.adapters.TeacherAdapter
+import com.google.firebase.firestore.toObject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.StringBuilder
 
 class pickTeacher : AppCompatActivity() {
     private lateinit var teachers : RecyclerView
@@ -14,18 +24,26 @@ class pickTeacher : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.pickdate)
-        val timeSlots = listOf(
-            "08:00-09:00",
-            "09:00-10:00",
-            "10:00-11:00",
-            "11:00-12:00",
-            "12:00-13:00",
-            "13:00-14:00"
-        )
+        setContentView(R.layout.pick_teacher)
+        private fun retrieveTeacher () = CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val querySnapshot = teacherCollectionRef.get().await()
+                val sb = StringBuilder()
+                for (document in querySnapshot.documents) {
+                    val teacher = document.toObject<TeacherModel>()
+                    sb.append("$teacher\n")
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+        }
         val data = timeSlots.map { ItemsViewModel(it)}
-        teachers = findViewById(R.id.recyclerView)
-        val adapter = CustomAdapter(data)
+        teachers = findViewById(R.id.teachers)
+        val adapter = TeacherAdapter(data)
         teachers.adapter = adapter
 
 
